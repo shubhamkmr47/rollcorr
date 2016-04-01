ededb76d0be3eb1c4c33a9bdd5dece8fde2dd634
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-04-02 03:16:43 +0530

    decision tree

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -1,27 +1,34 @@
 
 rollcorr <- function(){
   
-if (!require("gplots")) {
-  install.packages("gplots", dependencies = TRUE)
+# if (!require("gplots")) {
+#   install.packages("gplots", dependencies = TRUE)
+#   library(gplots)
+# }
+# if (!require("lattice")) {
+#   install.packages("lattice", dependencies = TRUE)
+#   library(lattice)
+# }
+# ### loading data
+# data(AirPassengers)
+# ### converting data
+# rowcolNames <- list(as.character(1949:1960), month.abb)
+# air_data <- matrix(AirPassengers,
+#                    ncol = 12,
+#                    byrow = TRUE,
+#                    dimnames = rowcolNames)
+# ### drawing heat maps
+# heatmap.2(air_data,
+#           trace = "none",
+#           density.info = "none",
+#           xlab = "month",
+#           ylab = "year",
+#           main = "Heatmap using opencpu")
+
+if (!require("party")) {
+  install.packages("party", dependencies = TRUE)
   library(gplots)
 }
-if (!require("lattice")) {
-  install.packages("lattice", dependencies = TRUE)
-  library(lattice)
-}
-### loading data
-data(AirPassengers)
-### converting data
-rowcolNames <- list(as.character(1949:1960), month.abb)
-air_data <- matrix(AirPassengers,
-                   ncol = 12,
-                   byrow = TRUE,
-                   dimnames = rowcolNames)
-### drawing heat maps
-heatmap.2(air_data,
-          trace = "none",
-          density.info = "none",
-          xlab = "month",
-          ylab = "year",
-          main = "Heatmap using opencpu")
+iris_ctree <- ctree(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data=iris)
+plot(iris_ctree)
 }
