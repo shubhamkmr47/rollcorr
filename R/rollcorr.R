862aef0b59063796329cfe6758d4671c9f844232
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-05-03 01:38:15 +0530

    interative scatter plot

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -32,9 +32,20 @@ rollcorr <- function(){
 # iris_ctree <- ctree(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data=iris)
 # plot(iris_ctree)
 
-idx <- sample(1:dim(iris)[1], 40)
-irisSample <- iris[idx,]
-irisSample$Species <- NULL
-hc <- hclust(dist(irisSample), method="ave")
-plot(hc, hang = -1, labels=iris$Species[idx], main = "Heirarichal Clustering", xlab = "IRIS dataset")
+# idx <- sample(1:dim(iris)[1], 40)
+# irisSample <- iris[idx,]
+# irisSample$Species <- NULL
+# hc <- hclust(dist(irisSample), method="ave")
+# plot(hc, hang = -1, labels=iris$Species[idx], main = "Heirarichal Clustering", xlab = "IRIS dataset")
+# }
+
+if (!require("plotly")) {
+  install.packages("plotly", dependencies = TRUE)
+  library(gplots)
 }
+
+library(plotly)
+set.seed(100)
+d <- diamonds[sample(nrow(diamonds), 1000), ]
+plot_ly(d, x = carat, y = price, text = paste("Clarity: ", clarity),
+        mode = "markers", color = carat, size = carat)
