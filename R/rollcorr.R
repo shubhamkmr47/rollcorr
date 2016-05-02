0363fb35c435d24db92de6e5b7b11f52b5824a3e
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-05-03 01:44:42 +0530

    solve plot issue

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -41,11 +41,9 @@ rollcorr <- function(){
 
 if (!require("plotly")) {
   install.packages("plotly", dependencies = TRUE)
-  library(gplots)
+  library(plotly)
 }
 
-library(plotly)
 set.seed(100)
 d <- diamonds[sample(nrow(diamonds), 1000), ]
-plot_ly(d, x = carat, y = price, text = paste("Clarity: ", clarity),
-        mode = "markers", color = carat, size = carat)
+plot_ly(d, x = carat, y = price, text = paste("Clarity: ", clarity), mode = "markers", color = carat, size = carat)
