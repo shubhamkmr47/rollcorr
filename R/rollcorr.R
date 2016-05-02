7c103c1e57bd197e062ac34b82cca577c1925141
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-05-03 01:53:58 +0530

    solve plot issue5

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -45,5 +45,6 @@ if (!require("plotly")) {
 }
 
 set.seed(100)
-d <- diamonds[sample(nrow(diamonds), 1000), ]
-    plot_ly(d, x = carat, y = price, text = paste("Clarity: ", clarity), mode = "markers", color = carat, size = carat)
+d <- diamonds[sample(nrow(diamonds), 1000), ];
+plot_ly(d, x = carat, y = price, text = paste("Clarity: ", clarity), mode = "markers", color = carat, size = carat)
+}
