ec37a670a943641ad8c820fd0efe2c1e639db5d9
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-05-23 01:09:07 +0530

    hclust7

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -1,42 +1,29 @@
 
 rollcorr <- function(){
   
-# if (!require("gplots")) {
-#   install.packages("gplots", dependencies = TRUE)
-#   library(gplots)
-# }
-# if (!require("lattice")) {
-#   install.packages("lattice", dependencies = TRUE)
-#   library(lattice)
-# }
-# ### loading data
-# data(AirPassengers)
-# ### converting data
-# rowcolNames <- list(as.character(1949:1960), month.abb)
-# air_data <- matrix(AirPassengers,
-#                    ncol = 12,
-#                    byrow = TRUE,
-#                    dimnames = rowcolNames)
-# ### drawing heat maps
-# heatmap.2(air_data,
-#           trace = "none",
-#           density.info = "none",
-#           xlab = "month",
-#           ylab = "year",
-#           main = "Heatmap using opencpu")
-
-if (!require("party")) {
-  install.packages("party", dependencies = TRUE)
-  library(party)
+if (!require("gplots")) {
+  install.packages("gplots", dependencies = TRUE)
+  library(gplots)
 }
-iris_ctree <- ctree(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data=iris)
-plot(iris_ctree)
-
-idx <- sample(1:dim(iris)[1], 40)
-irisSample <- iris[idx,]
-irisSample$Species <- NULL
-hc <- hclust(dist(irisSample), method="ave")
-plot(hc, hang = -1, labels=iris$Species[idx], main = "Heirarichal Clustering", xlab = "IRIS dataset")
+if (!require("lattice")) {
+  install.packages("lattice", dependencies = TRUE)
+  library(lattice)
+}
+### loading data
+data(AirPassengers)
+### converting data
+rowcolNames <- list(as.character(1949:1960), month.abb)
+air_data <- matrix(AirPassengers,
+                   ncol = 12,
+                   byrow = TRUE,
+                   dimnames = rowcolNames)
+### drawing heat maps
+heatmap.2(air_data,
+          trace = "none",
+          density.info = "none",
+          xlab = "month",
+          ylab = "year",
+          main = "Heatmap using opencpu")
 
 # if (!require("plotly")) {
 #     install.packages("plotly", dependencies = TRUE)
