4bbbf0565049480a2afb4a431756cde6d7811094
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-05-25 01:57:31 +0530

    json data

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -5,17 +5,32 @@ if (!require("d3heatmap")) {
   install.packages("d3heatmap", dependencies = TRUE)
   library(d3heatmap)
 }
+
+if (!require("RJSONIO")) {
+  install.packages("RJSONIO", dependencies = TRUE)
+  library(RJSONIO)
+}
   
-### loading data
-data(AirPassengers)
-### converting data
-rowcolNames <- list(as.character(1949:1960), month.abb)
-air_data <- matrix(AirPassengers,
-                   ncol = 12,
-                   byrow = TRUE,
-                   dimnames = rowcolNames)
+json_file <-  '[{"name":"Doe, John","group":"Red","age (y)":24,"height (cm)":182,"wieght (kg)":74.8,"score":null},
+    {"name":"Doe, Jane","group":"Green","age (y)":30,"height (cm)":170,"wieght (kg)":70.1,"score":500},
+    {"name":"Smith, Joan","group":"Yellow","age (y)":41,"height (cm)":169,"wieght (kg)":60,"score":null},
+    {"name":"Brown, Sam","group":"Green","age (y)":22,"height (cm)":183,"wieght (kg)":75,"score":865},
+    {"name":"Jones, Larry","group":"Green","age (y)":31,"height (cm)":178,"wieght (kg)":83.9,"score":221},
+    {"name":"Murray, Seth","group":"Red","age (y)":35,"height (cm)":172,"wieght (kg)":76.2,"score":413},
+    {"name":"Doe, Jane","group":"Yellow","age (y)":22,"height (cm)":164,"wieght (kg)":68,"score":902}]'
+
+
+json_file <- fromJSON(json_file)
+
+json_file <- lapply(json_file, function(x) {
+    x[sapply(x, is.null)] <- NA
+    unlist(x)
+})
+
+data <- do.call("rbind", json_file)
+
 ### drawing heat maps
-d3heatmap(air_data,
+heatmap(data,
           xlab = "month",
           ylab = "year",
           main = "Heatmap using opencpu")
