bdc8dcfcdf8468413b608f118f370217fa0fbe3f
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-05-25 02:03:24 +0530

    json data2

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -11,13 +11,13 @@ if (!require("RJSONIO")) {
   library(RJSONIO)
 }
   
-json_file <-  '[{"name":"Doe, John","group":"Red","age (y)":24,"height (cm)":182,"wieght (kg)":74.8,"score":null},
-    {"name":"Doe, Jane","group":"Green","age (y)":30,"height (cm)":170,"wieght (kg)":70.1,"score":500},
-    {"name":"Smith, Joan","group":"Yellow","age (y)":41,"height (cm)":169,"wieght (kg)":60,"score":null},
-    {"name":"Brown, Sam","group":"Green","age (y)":22,"height (cm)":183,"wieght (kg)":75,"score":865},
-    {"name":"Jones, Larry","group":"Green","age (y)":31,"height (cm)":178,"wieght (kg)":83.9,"score":221},
-    {"name":"Murray, Seth","group":"Red","age (y)":35,"height (cm)":172,"wieght (kg)":76.2,"score":413},
-    {"name":"Doe, Jane","group":"Yellow","age (y)":22,"height (cm)":164,"wieght (kg)":68,"score":902}]'
+json_file <-  '["age (y)":24,"height (cm)":182,"wieght (kg)":74.8,"score":null},
+    {"age (y)":30,"height (cm)":170,"wieght (kg)":70.1,"score":500},
+    {"age (y)":41,"height (cm)":169,"wieght (kg)":60,"score":null},
+    {"age (y)":22,"height (cm)":183,"wieght (kg)":75,"score":865},
+    {"age (y)":31,"height (cm)":178,"wieght (kg)":83.9,"score":221},
+    {"age (y)":35,"height (cm)":172,"wieght (kg)":76.2,"score":413},
+    {"age (y)":22,"height (cm)":164,"wieght (kg)":68,"score":902}]'
 
 
 json_file <- fromJSON(json_file)
