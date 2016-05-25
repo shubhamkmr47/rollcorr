49ef52ae4f0b2bf17afc96c3f2b6dc80c2c010e8
shubhamkmr47 <shubhamkmr47@gmail.com>
2016-05-25 23:20:30 +0530

    upload data

--- R/rollcorr.R
+++ R/rollcorr.R
@@ -1,5 +1,5 @@
 
-rollcorr <- function(){
+rollcorr <- function(data){
 
 if (!require("d3heatmap")) {
   install.packages("d3heatmap", dependencies = TRUE)
@@ -11,23 +11,6 @@ if (!require("RJSONIO")) {
   library(RJSONIO)
 }
   
-json_file <-  '[{"age (y)":24,"height (cm)":182,"wieght (kg)":74.8},
-    {"age (y)":30,"height (cm)":170,"wieght (kg)":70.1},
-    {"age (y)":41,"height (cm)":169,"wieght (kg)":60},
-    {"age (y)":22,"height (cm)":183,"wieght (kg)":75},
-    {"age (y)":31,"height (cm)":178,"wieght (kg)":83.9},
-    {"age (y)":35,"height (cm)":172,"wieght (kg)":76.2},
-    {"age (y)":22,"height (cm)":164,"wieght (kg)":68}]'
-
-
-json_file <- fromJSON(json_file)
-
-json_file <- lapply(json_file, function(x) {
-    x[sapply(x, is.null)] <- NA
-    unlist(x)
-})
-
-data <- do.call("rbind", json_file)
 data <- data.matrix(data)
 
 ### drawing heat maps
